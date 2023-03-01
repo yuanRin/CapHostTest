7aab68e47cefd65e97b943e25a1e283b8946dc82
yuanRin <122252316+yuanRin@users.noreply.github.com>
2023-03-01 13:05:35 +0800

    testing 1.03010323
    first look of UI

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,56 +1,13 @@
 <script setup>
-import WelcomeItem from './WelcomeItem.vue'
-//import DocumentationIcon from './icons/IconDocumentation.vue'
-//import ToolingIcon from './icons/IconTooling.vue'
-//import EcosystemIcon from './icons/IconEcosystem.vue'
-//import CommunityIcon from './icons/IconCommunity.vue'
-//import SupportIcon from './icons/IconSupport.vue'
+import Section1 from './section/Section1.vue'
+import Section2 from './section/Section2.vue'
+import Section3 from './section/Section3.vue'
 </script>
 
 <template>
-  <WelcomeItem>
-  <template #header>
-    <h1>Test Configuration</h1>
-    <div>
-    <label for="cbTestFrequency">Test Frequency : </label>
-    <select id="cbTestFrequency" v-model="selectedOption1">
-        <option value="option1">Option 1</option>
-        <option value="option2">Option 2</option>
-        <option value="option3">Option 3</option>
-    </select>
-    </div>
-    <div>
-      <label for="cbTestVoltage">Voltage Level : </label>
-      <select id="cbTestVoltage">
-        <option value="option1">Option 1</option>
-        <option value="option2">Option 2</option>
-        <option value="option3">Option 3</option>
-      </select>
-    </div>
-  </template>
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #header>
-      <h1>Calibration</h1>
-      <div>
-        <button @click="handleButtonClick">Open Calibration</button>
-        <button>Short Calibration</button>
-      </div>
-      <label>NOTE: Warm up for 30 minutes before do calibration.</label>
-    </template>
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #header>
-      <h1>Result</h1>
-      <div>
-        <label>Some label</label>
-        <label>Another label</label>
-        <label>Yet another label</label>
-      </div>
-    </template>
-  </WelcomeItem>
-  
-
+  <main>
+    <Section1 />
+    <Section2 />
+    <Section3 />
+  </main>
 </template>
---
+++ src/components/section/Section1.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class = "item">
+    <h1 class="font h1">Test Configuration</h1>
+    <div>
+    <label for="cbTestFrequency" class="font">Test Frequency : </label>
+    <select id="cbTestFrequency" class="bigcombobox">
+        <option value="option1" class="font">1 MHz</option>
+        <option value="option2" class="font">1 KHz</option>
+        <option value="option3" class="font">120 Hz</option>
+    </select>
+    </div>
+    <div>
+      <label for="cbTestVoltage" class="font">Voltage Level</label>
+      <label for="cbTestVoltage" class="font lbl "> :</label>
+      <select id="cbTestVoltage" class="bigcombobox">
+        <option value="option1" class="font">0.25 Vrms</option>
+        <option value="option2" class="font">0.5 Vrms</option>
+        <option value="option3" class="font">1 Vrms</option>
+        <option value="option3" class="font">2 Vrms</option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.font {
+  font-size: 1rem;
+  font-weight: 500;
+  color: var(--color-heading);
+}
+
+.h1 {
+  font-size: 1.8rem;
+}
+
+.lbl {
+  display: "inline-block";
+  margin-left: .8rem;
+  margin-right: .2rem;
+}
+
+.bigcombobox{
+  width: 10rem;
+  height: 1.8rem;
+  margin-left: .5rem;
+  margin-top: .3rem;
+  cursor: pointer;
+}
+
+@media (min-width: 1024px) {
+  .item {
+    margin-top: 0;
+    padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
+  }
+
+  i {
+    top: calc(50% - 25px);
+    left: -26px;
+    position: absolute;
+    border: 1px solid var(--color-border);
+    background: var(--color-background);
+    border-radius: 8px;
+    width: 50px;
+    height: 50px;
+  }
+
+  .item:before {
+    content: ' ';
+    border-left: 1px solid var(--color-border);
+    position: absolute;
+    left: 0;
+    bottom: calc(50% + 25px);
+    height: calc(50% - 25px);
+  }
+
+  .item:after {
+    content: ' ';
+    border-left: 1px solid var(--color-border);
+    position: absolute;
+    left: 0;
+    top: calc(50% + 25px);
+    height: calc(50% - 25px);
+  }
+
+  .item:first-of-type:before {
+    display: none;
+  }
+
+  .item:last-of-type:after {
+    display: none;
+  }
+}
+</style>
+
+  
